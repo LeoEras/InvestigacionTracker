e40b92e59765b4bdc58a1617ee6f464f9853d169
Leonardo Eras <leoded94@hotmail.com>
2016-09-08 11:32:26 -0500

    Adaptacion a maquinas virtuales

--- script.cmd
+++ script.cmd
@@ -1,9 +1,9 @@
 @echo off
 set manic="C:\Program Files (x86)\ManicTime"
 cd %manic%
-mtc backup "C:\Users\estudiante\Desktop" /f:"bck.sdf" /o
-set sqlcsv="C:\Users\estudiante\Desktop\Scripts"
-set datasource="Data Source=C:\Users\estudiante\Desktop\bck.sdf"
+mtc backup "C:\Users\estudiante.2016\Desktop" /f:"bck.sdf" /o
+set sqlcsv="C:\Users\estudiante.2016\Desktop\Scripts"
+set datasource="Data Source=C:\Users\estudiante.2016\Desktop\bck.sdf"
 set query="select a.ActivityId, a.DisplayName as Event, a.StartLocalTime, a.EndLocalTime, g.DisplayName as Application, t.TypeName as Type from Activity a, [Group] g, Timeline t where a.GroupId = g.GroupId and a.TimelineId = t.TimelineId order by a.ActivityId;"
 set output="output.csv"
 cd %sqlcsv%
