cbea72b3868c4a59bd43612e1b69f239a57509fc
LeoEras <leoded94@hotmail.com>
2017-05-20 13:59:43 -0500

    Todo en la misma base

--- createAll.sql
+++ createAll.sql
@@ -6,6 +6,7 @@ use investigacion;
 
 CREATE TABLE if not exists Datos  (
     id INT primary KEY auto_increment,
+    periodo INT,
     actividad varchar(300),
     dia_inicio date,
     hora_inicio time,
@@ -26,130 +27,238 @@ LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado23.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado24.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado25.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado26.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado27.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado28.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado29.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado30.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado31.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado32.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado33.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado34.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado35.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado36.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado37.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado38.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado39.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado40.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado41.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado42.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-1T/depurado43.csv' INTO TABLE datos
 FIELDS TERMINATED BY '|' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 (actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
-set id = null;
+set id = null, periodo = 1;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado1.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado2.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado3.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado4.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado5.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado6.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado7.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado8.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado9.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado10.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado13.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado14.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado15.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado17.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado18.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado21.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado22.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/2016-2T/depurado24.csv' INTO TABLE datos
+FIELDS TERMINATED BY '|' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(actividad, dia_inicio, hora_inicio, dia_final, hora_final, tiempo, responsable, clase, importancia, usuario)
+set id = null, periodo = 2;
